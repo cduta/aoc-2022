370d9bbf1a96672b6d1bf860453beab31282f089
Christian Duta <christianmduta@gmail.com>
2022-11-27 14:08:34 +0100

    Add argument parser and logger

---
+++ common/src/lib.rs
@@ -0,0 +1,61 @@
+pub mod args {
+  use std::fmt;
+  use clap::Parser;  
+
+  #[derive(clap::ValueEnum, Clone, Debug)]
+  pub enum Part { One, Two }
+
+  impl fmt::Display for Part {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+          Part::One => write!(f, "One"),
+          Part::Two => write!(f, "Two")
+        }
+    }
+  }
+
+  #[derive(Parser)]
+  pub struct Args {
+    #[arg(short, long)]
+    pub input: String,
+
+    #[arg(short, long, default_value = "one")]
+    pub part: Part,
+
+    #[arg(short, long, default_value = "log-config.yml")]
+    pub log: String
+  }
+
+  impl Args {
+      pub fn populate() -> Args {
+          Args::parse()
+      }
+  }
+
+  #[cfg(test)]
+  mod tests {
+      use super::*;
+
+      #[test]
+      fn display_part_one() {
+          assert_eq!(Part::One.to_str(), "one");
+      }
+
+      #[test]
+      fn display_part_two() {
+          assert_eq!(Part::Two.to_str(), "two");
+      }
+  }
+}
+
+pub mod logger {
+  use log4rs;
+  use anyhow;
+
+  pub fn initialize(log: String) -> Result<(),anyhow::Error> {
+    log4rs::init_file(&log, Default::default()).or_else(
+      |err| {
+        println!("Failed to read {}: {}", &log, err.to_string());
+        Err(err)})
+  }
+}
--- day00/src/main.rs
+++ day00/src/main.rs
@@ -1,21 +1,15 @@
-use std::env;
-use std::io;
-use std::path;
-
-fn pathbuf_to_string(buf: io::Result<path::PathBuf>) -> Result<String, io::Error> {
-  buf.and_then(
-    |buf| buf.to_str().ok_or(io::Error::new(io::ErrorKind::Other, "to_str() failed.")).and_then(
-      |str| Ok(str.to_string())))
-}
-
-fn print_path(path: Result<String, io::Error>) {
-  match path {
-    Ok(p)  => println!("Hello, world from {}!", p),
-    Err(e) => println!("Error when reading current dir: {}", e.to_string())
-  }
-}
+use common::args::Args;
+use common::logger::initialize;
+use log::debug;
 
 fn main() {
-  print_path(pathbuf_to_string(Err(io::Error::new(io::ErrorKind::Other, "Could not read current dir."))));
-  print_path(pathbuf_to_string(env::current_dir()));
+  let args: Args = Args::populate();
+  let result = initialize(args.log.clone());
+  if result.is_err() {
+    panic!("Preparation failed!");
+  }
+  trace!("Begin logging for {}", env!("CARGO_PKG_NAME"));
+  trace!("Parsing input...");
+
+  info!("Input: {} and Part: {}", args.input, args.part);
 }
