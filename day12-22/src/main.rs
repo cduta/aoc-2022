23106405984fb58cc40704a478674db03b60d755
Christian Duta <christianmduta@gmail.com>
2022-12-12 18:10:20 +0100

    Day 12 of 2022 (part two)

--- day12-22/src/main.rs
+++ day12-22/src/main.rs
@@ -68,7 +68,7 @@ fn adjacent(node: Node, width: usize, height: usize) -> Vec<Node> {
   ).filter_map(|node_option| node_option).collect()
 }
 
-fn astar(height_map: HeightMap, start: Node, end: Node) -> Option<Vec<Node>> {
+fn astar(height_map: &HeightMap, start: Node, end: Node, memo: &mut HashMap<Node, Vec<Node>>) -> Option<Vec<Node>> {
   let mut unvisited = DoublePriorityQueue::new();
   unvisited.push((None, start), 0);
 
@@ -84,7 +84,8 @@ fn astar(height_map: HeightMap, start: Node, end: Node) -> Option<Vec<Node>> {
         let next_steps = steps+1+height_map.manhattan_distance(&next, &end);
         let visited_option = visited.get(&next);
         if (visited_option.is_none() || visited_option.is_some() && next_steps < visited_option.unwrap().1) && 
-           (next_height == 0 || curr_height >= next_height-1) {
+           (next_height == 0 || curr_height >= next_height-1) && 
+           !memo.contains_key(&next) {
           unvisited.push((Some(curr), next), steps+1);
         }
       }
@@ -102,21 +103,46 @@ fn astar(height_map: HeightMap, start: Node, end: Node) -> Option<Vec<Node>> {
   let mut curr = &end;
   
   while let Some((Some(prev),_)) = visited.get(curr) {
+    if height_map.height_at(curr) == 0 {
+      memo.insert(*curr, path.clone().into_iter().rev().collect());
+    }
     path.push(*prev);
     curr = prev;
   }
 
+  path.reverse();
   return if visited.contains_key(&end) {Some(path)} else {None};
 }
 
 fn one(input: &Input) -> String {
   let height_map = HeightMap::new(&input.lines);
   let (start, end) = (height_map.start, height_map.end);
-  return if let Some(path) = astar(height_map, start, end) {path.len().to_string()} else {"Unreachable".to_string()};
+  let mut memo = HashMap::new();
+  return if let Some(path) = astar(&height_map, start, end, &mut memo) {(path.len()-1).to_string()} else {"Unreachable".to_string()};
 }
 
-fn two(_input: &Input) -> String {
-  return "42".to_string();
+fn two(input: &Input) -> String {
+  let height_map = HeightMap::new(&input.lines);
+  let (_, end) = (height_map.start, height_map.end);
+  let mut memo = HashMap::new();
+
+  height_map.map.iter().enumerate().for_each(
+    |(y,row)| {
+      row.iter().enumerate().for_each(
+        |(x,height)| {
+          if *height == 0 {
+            astar(&height_map, (x,y), end, &mut memo);
+          }
+        }
+      );
+    }
+  );
+
+  let mut paths: Vec<Vec<Node>> = memo.into_iter().filter(|(first, _)| height_map.height_at(first) == 0).map(|(_,path)| path).collect();
+
+  paths.sort_by(|a,b| a.len().cmp(&b.len()));
+
+  return (paths.first().unwrap().len()-1).to_string();
 }
 
 fn main() {
