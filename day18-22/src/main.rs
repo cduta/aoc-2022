acf0ba7982ee72edd68c78f027e1d3ea73e7e846
Christian Duta <christianmduta@gmail.com>
2022-12-19 10:10:57 +0100

    Day 18 of 2022

--- day18-22/src/main.rs
+++ day18-22/src/main.rs
@@ -8,6 +8,7 @@ use std::time::Instant;
 type Int = i64;
 type Square = (Int,Int);
 type Cube = (Int,Int,Int);
+type Dims = (Int,Int,Int,Int,Int,Int);
 
 enum Axis {X, Y, Z}
 
@@ -53,15 +54,21 @@ fn scan_surface(cubes: &HashSet<Cube>, start: Int, end: Int, reversed: bool, axi
   return result;
 }
 
-fn one(input: &Input) -> String {
-  let cubes = prepare(&input.lines);
 
+fn dimensions(cubes: &HashSet<Cube>) -> Dims {
   let back   = cubes.iter().min_by(|(_,_,z1), (_,_,z2)| z1.cmp(z2)).unwrap().2;
   let front  = cubes.iter().max_by(|(_,_,z1), (_,_,z2)| z1.cmp(z2)).unwrap().2;
   let bottom = cubes.iter().min_by(|(_,y1,_), (_,y2,_)| y1.cmp(y2)).unwrap().1;
   let top    = cubes.iter().max_by(|(_,y1,_), (_,y2,_)| y1.cmp(y2)).unwrap().1;
   let left   = cubes.iter().min_by(|(x1,_,_), (x2,_,_)| x1.cmp(x2)).unwrap().0;
   let right  = cubes.iter().max_by(|(x1,_,_), (x2,_,_)| x1.cmp(x2)).unwrap().0;
+
+  return (back, front, bottom, top, left, right);
+}
+
+fn one(input: &Input) -> String {
+  let cubes = prepare(&input.lines);
+  let (back, front, bottom, top, left, right) = dimensions(&cubes);
 
   let mut total = 0;
 
@@ -75,8 +82,49 @@ fn one(input: &Input) -> String {
   return total.to_string();
 }
 
-fn two(_input: &Input) -> String {
-  return "42".to_string();
+fn two(input: &Input) -> String {
+  fn neighbors((x,y,z): &Cube, (back, front, bottom, top, left, right): &Dims) -> Vec<Cube> {
+    let mut result = vec![];
+    if x >= left {
+      result.push((*x-1,*y,*z));
+    }  
+    if x <= right {
+      result.push((*x+1,*y,*z));
+    }  
+    if y >= bottom {
+      result.push((*x,*y-1,*z));
+    }  
+    if y <= top {
+      result.push((*x,*y+1,*z));
+    }  
+    if z >= back {
+      result.push((*x,*y,*z-1));
+    }  
+    if z <= front {
+      result.push((*x,*y,*z+1));
+    }  
+    return result;
+  }
+
+  let cubes = prepare(&input.lines);
+  let dims@(back, _, bottom, _, left, _) = dimensions(&cubes);
+
+  let mut total = 0;
+  let mut to_visit = vec![(left-1, bottom-1, back-1)];
+  let mut visited  = vec![(left-1, bottom-1, back-1)].into_iter().collect::<HashSet<Cube>>();
+
+  while let Some((x,y,z)) = to_visit.pop() {
+    for neighbor in neighbors(&(x,y,z), &dims) {
+      if cubes.contains(&neighbor) {
+        total += 1;
+      } else if !visited.contains(&neighbor) {
+        visited.insert(neighbor);
+        to_visit.push(neighbor);
+      }
+    }
+  }
+
+  return total.to_string();
 }
 
 fn main() {
