88c227862ae5fe24717c887dc0bb663159a9d962
Christian Duta <christianmduta@gmail.com>
2022-12-25 08:50:07 +0100

    Day 24 and 25 stubs added

---
+++ day24-22/src/main.rs
@@ -0,0 +1,34 @@
+// ## TODO ##
+use common::Input;
+use common::args::Part;
+use common::init::{startup, print, shutdown};
+use log::{trace,info};
+use std::time::Instant;
+
+fn one(_input: &Input) -> String {
+  return "42".to_string();
+}
+
+fn two(_input: &Input) -> String {
+  return "42".to_string();
+}
+
+fn main() {
+  let day  : String = env!("CARGO_PKG_NAME").to_string();
+  let input: Input = startup();
+
+  if input.verbose {
+    trace!("Running {} (Part {}) with input:\n{}", env!("CARGO_PKG_NAME"), input.part, input.lines.join("\n"));
+  }
+
+  let f = match input.part {
+    Part::One => one,
+    Part::Two => two
+  };
+
+  let start = Instant::now();
+  print(day, input.part.clone(), f(&input));
+  info!("Time elapsed: {} ms", start.elapsed().as_millis());
+
+  shutdown();
+}
